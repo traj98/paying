e265184d65dfb782040510403191608c75e757aa
rahult016 <rahult016@gmail.com>
2020-05-11 18:52:48 +0530

    list_display:

--- paying_guest/accounts/admin.py
+++ paying_guest/accounts/admin.py
@@ -6,8 +6,11 @@ from django.contrib import admin
 # Register your models here.
 from . models import *
 
-class AdminUserProfile(admin.ModelAdmin):
-	lsit_display = ['firstname','lastname','phone','sex']
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('firstname','lastname','phone','sex')
+	search_fields =('firstname','lastname')
+	
+	list_per_page = 25
 
 
-admin.site.register(UserProfile,AdminUserProfile)
+admin.site.register(UserProfile,ProfileAdmin)
